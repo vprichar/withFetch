c960949dcd1b048467489a571c030ed2e184b085
Theo Anderson <theoanderson3@gmail.com>
2017-11-24 20:41:04 -0500

    simple with fetch component

---
+++ withFetch.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+const withFetch = (url) => {
+	return (Component) => {
+		return class withFetchedData extends Component {
+			constructor(props) {
+				super(props);
+				this.state = {
+					data: []
+				}
+			}
+			componentDidMount() {
+				fetch(url)
+					.then(r => r.json())
+					.then(data => this.setState({ data }))
+					.catch(error => console.log(error))
+			}
+			render() {
+				const { data } = this.state;
+				return (
+					<Component
+						data={data}
+						{...this.props}
+						{...this.state}
+					/>
+				)
+			}
+		}
+	}
+}
+
+export {
+	withFetch
+}
