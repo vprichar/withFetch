a9962183d5af0068f33c4b23b915f3cb28b7dc20
Theo Anderson <theoanderson3@gmail.com>
2017-11-24 22:17:20 -0500

    fix formatting

--- withFetch.js
+++ withFetch.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const withFetch = (url) => {
 	return (Component) => {
-		return class withFetchedData extends Component {
+		return class withFetchedData extends React.Component {
 			constructor(props) {
 				super(props);
 				this.state = {
