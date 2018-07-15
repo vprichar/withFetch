2e8ddb7ee1d175fd9ddadcd331f54a0de78550dc
Theo Anderson <theoanderson3@gmail.com>
2018-07-15 09:36:48 -0700

    formatting

--- withFetch.js
+++ withFetch.js
@@ -1,34 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const withFetch = (url) => {
-	return (Component) => {
-		return class withFetchedData extends React.Component {
-			constructor(props) {
-				super(props);
-				this.state = {
-					data: []
-				}
-			}
-			componentDidMount() {
-				fetch(url)
-					.then(r => r.json())
-					.then(data => this.setState({ data }))
-					.catch(error => console.log(error))
-			}
-			render() {
-				const { data } = this.state;
-				return (
-					<Component
-						data={data}
-						{...this.props}
-						{...this.state}
-					/>
-				)
-			}
-		}
-	}
-}
+const withFetch = url => {
+  return Component => {
+    return class withFetchedData extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          data: []
+        };
+      }
+      componentDidMount() {
+        fetch(url)
+          .then(r => r.json())
+          .then(data => this.setState({ data }))
+          .catch(error => console.log(error));
+      }
+      render() {
+        const { data } = this.state;
+        return <Component data={data} {...this.props} {...this.state} />;
+      }
+    };
+  };
+};
 
-export {
-	withFetch
-}
+export { withFetch };
